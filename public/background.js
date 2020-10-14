b50eb76d316d5df23fd9376ba2ea94fafb70578f
aryzhikov <a.ryzhikov@dodopizza.com>
2020-10-14 19:10:48 +0300

    notifications

--- public/background.js
+++ public/background.js
@@ -1,16 +1,20 @@
-const interval = 15 * 60 * 1000;
-
-const delta = 0.15;
-const jitter = 1 - delta + delta * 2 * Math.random();
-
-setInterval(() => {
-  chrome.notifications.create({
-    type: "basic",
-    iconUrl: "favicon.png",
-    title: "Слышь",
-    message: "Ну расскажи себе, что делаешь",
-  });
-}, interval * jitter);
-
+const delayInMinutes = 5;
+const connectedKey = "slysh.arrived";
 // todo: render reminder notification only if connected = true in chrome.storage
 // todo: play with interval in case I missed reminder a couple of times
+
+chrome.alarms.create({ delayInMinutes: delayInMinutes });
+
+chrome.alarms.onAlarm.addListener(() => {
+  chrome.storage.sync.get([connectedKey], (items) => {
+    console.log(connectedKey, items[connectedKey]);
+    if (items[connectedKey] === true) {
+      chrome.notifications.create({
+        type: "basic",
+        iconUrl: "favicon.png",
+        title: "Слышь",
+        message: "Ну расскажи себе, что делаешь",
+      });
+    }
+  });
+});
